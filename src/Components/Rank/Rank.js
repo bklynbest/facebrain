045566b0393da585c09a70324d10ed058abb07f6
Jonathan Momperousse <bklynbest718@gmail.com>
2019-02-16 23:29:51 +0000

    created main UI components

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Rank = () => {
+    return (
+        <div>
+            <div className="white f3">
+                {'Jonathan your current rank is...' }
+            </div>
+            <div className="white f5">
+                {'# 43'}
+            </div>
+        </div>
+        
+    )
+}
+
+export default Rank;
