426af1b5cfa5ee63b69c8fddf2ee64287e4bc6ab
Jonathan Momperousse <bklynbest718@gmail.com>
2019-02-20 07:52:43 +0000

    added routing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 // eslint-disable-next-line
 import SignIn from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
 import Rank from './Components/Rank/Rank';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
@@ -37,7 +38,7 @@ class App extends Component {
     };
   }
   
-  calculateFaceLocation = (data) => {
+    calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
@@ -49,48 +50,76 @@ class App extends Component {
       bottomRow: height - (clarifaiFace.bottom_row * height)
     }
   }
-  
+
   displayFaceBox = (box) => {
     this.setState({box: box});
   }
-  
-  
-  
-  onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input})
-      app.models
-        .predict(
-          Clarifai.FACE_DETECT_MODEL, this.state.input)
-          .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-          .catch(err => console.log(err));
-  }
-  
-  onRouteChange = () => {
-    this.setState({route: 'home'})
-  }
-  
+
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
-  
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <Particles className="particles" 
-                params={particlesOptions}
+         <Particles className='particles'
+          params={particlesOptions}
         />
-        <Navigation />
-        { this.state.route === 'signin' 
-        ?<SignIn onRouteChange = {this.onRouteChange}/>
-        :<div>
-          <Logo />
-          <Rank />
-          <ImageLinkForm 
-            onInputChange={this.onInputChange} 
-            onButtonSubmit={this.onButtonSubmit} 
-          />
-          <FaceRecognition box={this.state.box} imageUrl= {this.state.imageUrl}/>
-        </div>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
+          ? <div>
+              <Logo />
+              <Rank
+                name={this.state.user.name}
+                entries={this.state.user.entries}
+              />
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl} />
+            </div>
+          : (
+             route === 'signin'
+             ? <SignIn onRouteChange = {this.onRouteChange}/>
+             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            )
         }
       </div>
     );
